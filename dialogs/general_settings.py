6f58978863fa02201c82b4c1d652cc743ca0ac93
Walz 194 <67635601+Walz194@users.noreply.github.com>
2022-01-03 16:10:06 +0100

    cleaned up the code and made the close button work

--- dialogs/general_settings.py
+++ dialogs/general_settings.py
@@ -1,17 +1,13 @@
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QListWidget, QVBoxLayout, QLabel, QSizePolicy, \
-    QPushButton, QComboBox, QLineEdit
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QListWidget, QVBoxLayout, QLabel, QSizePolicy, QPushButton, QComboBox, QLineEdit
 
 
 class GeneralSettingsWidget(QWidget):
 
     def __init__(self):
         super().__init__()
-        # self.tab_content = QWidget()
-        # self.horizontalLayout_7 = QHBoxLayout(self.tab_content)
         self.horizontalLayout_7 = QHBoxLayout(self)
         self.left_side_wrap = QVBoxLayout(self)
-        # self.data_category = QLabel(self.tab_content)
         self.data_category = QLabel(self)
         self.data_category.setText('Data Category')
 
@@ -19,25 +15,7 @@ class GeneralSettingsWidget(QWidget):
         font.setPointSize(11)
         self.data_category.setFont(font)
         self.left_side_wrap.addWidget(self.data_category)
-        # self.data_category_list = QListWidget(self.tab_content)
         self.data_category_list = QListWidget(self)
-
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_list.addItem(item)
 
         self.data_category_list.addItem("Admit Type");
         self.data_category_list.addItem("Arrival Mode");
@@ -52,7 +30,6 @@ class GeneralSettingsWidget(QWidget):
 
         self.right_side_wrap = QVBoxLayout(self)
         self.data_category_options_wrap = QVBoxLayout(self)
-        # self.data_category_options = QLabel(self.tab_content)
         self.data_category_options = QLabel(self)
         self.data_category_options.setText('Account Status')
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
@@ -65,14 +42,7 @@ class GeneralSettingsWidget(QWidget):
         font.setPointSize(11)
         self.data_category_options.setFont(font)
         self.data_category_options_wrap.addWidget(self.data_category_options)
-        # self.data_category_options_list = QListWidget(self.tab_content)
         self.data_category_options_list = QListWidget(self)
-        # item = QListWidgetItem()
-        # self.data_category_options_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_options_list.addItem(item)
-        # item = QListWidgetItem()
-        # self.data_category_options_list.addItem(item)
 
         self.data_category_options_list.addItem("Active");
         self.data_category_options_list.addItem("Inactive");
@@ -83,71 +53,57 @@ class GeneralSettingsWidget(QWidget):
         self.input_form_wrap = QVBoxLayout(self)
 
         self.data_id_and_edit_wrap = QHBoxLayout(self)
-        # self.data_id = QLabel(self.tab_content)
         self.data_id = QLabel(self)
         self.data_id.setMinimumSize(QtCore.QSize(100, 0))
         self.data_id.setMaximumSize(QtCore.QSize(100, 16777215))
         self.data_id_and_edit_wrap.addWidget(self.data_id)
-        # self.data_id_entry = QLineEdit(self.tab_content)
         self.data_id_entry = QLineEdit(self)
         self.data_id_and_edit_wrap.addWidget(self.data_id_entry)
-        # self.edit_change_btn = QPushButton(self.tab_content)
         self.edit_change_btn = QPushButton(self)
         self.data_id_and_edit_wrap.addWidget(self.edit_change_btn)
         self.input_form_wrap.addLayout(self.data_id_and_edit_wrap)
 
         self.description_wrap = QHBoxLayout(self)
-        # self.description = QLabel(self.tab_content)
         self.description = QLabel(self)
         self.description.setMinimumSize(QtCore.QSize(100, 0))
         self.description.setMaximumSize(QtCore.QSize(100, 16777215))
         self.description_wrap.addWidget(self.description)
-        # self.description_entry = QLineEdit(self.tab_content)
         self.description_entry = QLineEdit(self)
         self.description_wrap.addWidget(self.description_entry)
         self.input_form_wrap.addLayout(self.description_wrap)
 
         self.parent_data_wrap = QHBoxLayout(self)
-        # self.parent_data = QLabel(self.tab_content)
         self.parent_data = QLabel(self)
         self.parent_data.setMinimumSize(QtCore.QSize(100, 0))
         self.parent_data.setMaximumSize(QtCore.QSize(100, 16777215))
         self.parent_data_wrap.addWidget(self.parent_data)
-        # self.parent_data_options = QComboBox(self.tab_content)
         self.parent_data_options = QComboBox(self)
         self.parent_data_wrap.addWidget(self.parent_data_options)
         self.input_form_wrap.addLayout(self.parent_data_wrap)
 
         self.status_wrap = QHBoxLayout(self)
-        # self.status = QLabel(self.tab_content)
         self.status = QLabel(self)
         self.status.setMinimumSize(QtCore.QSize(100, 0))
         self.status.setMaximumSize(QtCore.QSize(100, 16777215))
         self.status_wrap.addWidget(self.status)
-        # self.status_options = QComboBox(self.tab_content)
         self.status_options = QComboBox(self)
         self.status_wrap.addWidget(self.status_options)
         self.input_form_wrap.addLayout(self.status_wrap)
 
         self.locked_wrap = QHBoxLayout(self)
-        # self.locked = QLabel(self.tab_content)
         self.locked = QLabel(self)
         self.locked.setMinimumSize(QtCore.QSize(100, 0))
         self.locked.setMaximumSize(QtCore.QSize(100, 16777215))
         self.locked_wrap.addWidget(self.locked)
-        # self.locked_options = QComboBox(self.tab_content)
         self.locked_options = QComboBox(self)
         self.locked_wrap.addWidget(self.locked_options)
         self.input_form_wrap.addLayout(self.locked_wrap)
 
         self.buttons_wrap = QHBoxLayout(self)
-        # self.save_btn = QPushButton(self.tab_content)
         self.save_btn = QPushButton(self)
         self.buttons_wrap.addWidget(self.save_btn)
-        # self.clear_btn = QPushButton(self.tab_content)
         self.clear_btn = QPushButton(self)
         self.buttons_wrap.addWidget(self.clear_btn)
-        # self.close_btn = QPushButton(self.tab_content)
         self.close_btn = QPushButton(self)
         self.buttons_wrap.addWidget(self.close_btn)
         self.input_form_wrap.addLayout(self.buttons_wrap)
@@ -164,12 +120,3 @@ class GeneralSettingsWidget(QWidget):
         self.clear_btn.setText("Clear")
         self.close_btn.setText("Close")
 
-        #
-        # self.tab_two = QWidget()
-        # self.tabWidget.addTab(self.tab_two, "")
-        # self.verticalLayout_5.addWidget(self.tabWidget)
-
-        # self.retranslateUi(MainWindow)
-        # self.tabWidget.setCurrentIndex(0)
-        # QtCore.QMetaObject.connectSlotsByName(MainWindow)'
-        # self.tab_content.show()
--- tabs_area.py
+++ tabs_area.py
@@ -1,9 +1,4 @@
-from PyQt5 import QtCore
-from PyQt5.QtCore import QCoreApplication, QMetaObject, QObject, QPoint,\
-    QRect, QSize, QUrl, Qt
-from PyQt5.QtGui import QBrush, QColor, QConicalGradient, QCursor, QFont,\
-    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,\
-    QRadialGradient
+from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import *
 from dialogs import dialog_maps
 
@@ -13,30 +8,12 @@ class TabsArea:
     def __init__(self,parent,layout):
         self.p = parent
         self.right_tabwidget = QTabWidget(parent)
+        self.right_tabwidget.setMinimumSize(QSize(654, 0))
         self.right_tabwidget.setTabsClosable(True)
-        self.init_UI()
+
+        # Code to ensure that the tab is destroyed on click
+        self.right_tabwidget.tabCloseRequested.connect(lambda index: self.right_tabwidget.removeTab(index))
         layout.addWidget(self.right_tabwidget, 1, 1, 1, 1)
 
-
-
-    def tabChanged(self):
-        print(f'Tab was changed to : {self.right_tabwidget.currentIndex()}')
-
-
-    def init_UI(self):
-        self.right_tabwidget.setMinimumSize(QSize(654, 0))
-        # self.right_tabwidget.addTab(self.tab_1, "One")
-        # self.right_tabwidget.addTab(self.tab_2, "Two")
-
-        # print(self.right_tabwidget.tabText(1))
-
-
-    def changeTab(self):
-        # setTabText(index of the tab, tab name)
-        self.right_tabwidget.setTabText(0,'Alrighty')
-
     def addTabs(self,text):
-        # self.right_tabwidget.addTab(self.right_tabwidget,text)
-        # self.right_tabwidget.
-        # print(text)
         dialog_maps.on_tree_item_clicked(self.right_tabwidget,text)
